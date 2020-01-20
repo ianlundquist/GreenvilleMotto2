ef842f2be9e8a1ef9262d36fd3d461ec53ced23b
Ian Lundquist <oe1550uh@go.minnstate.edu>
2020-01-19 18:28:43 -0600

    Add project files.

---
+++ GreenvilleMotto2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+  
+ 
+ */
+
+using System;
+
+namespace GreenvilleMotto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
